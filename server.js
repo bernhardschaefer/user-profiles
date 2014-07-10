d0a206645e30340f4cfe30cae7697d86bf1716c8
bernhardschaefer <bschaefer87@gmx.net>
2014-07-10 20:45:32 +0200

    extract express app from server.js to enable testing with separate database

--- server.js
+++ server.js
@@ -1,36 +1,14 @@
+/**
+ * User profile app entry point
+ */
 
-// Module dependencies
-var express = require('express'); //Web framework
-var app = express();
-var bodyParser = require('body-parser');
-var logger = require('morgan');
 var mongoose = require('mongoose'); //MongoDB integration
+var app = require('./app');
 
-var user = require('./routes/users');
-
-// Configure server
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(logger()); // placed before static files middleware so that all requests are logged
-app.use(express.static(__dirname + '/public'));
-
-// Configure Database
+// Connect to Database
 mongoose.connect('mongodb://localhost/userProfiles');
 
-// Routing
-var router = express.Router();
-
-router.get('/users', user.readUsers);
-router.post('/users', user.createUser);
-router.get('/users/:id', user.readSingleUser);
-router.put('/users/:id', user.updateUser);
-router.delete('/users/:id', user.deleteUser);
-
-app.use('/api', router);
-
-//Start server
+// Start server
 var port = 3000;
 app.listen(port, function() {
   console.log('Express server listening on port %d in %s mode', port, app.settings.env);
